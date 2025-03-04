3ce890e43f899ca625fa2bff5c03e366ecea397e
Nicholas Crothers <hawx523@gmail.com>
2025-03-04 10:33:53 -0600

    Add parsing benchmark

---
+++ behaviortree-rs/benches/parsing.rs
@@ -0,0 +1,37 @@
+mod nodes;
+mod trees;
+
+use std::time::Instant;
+
+use behaviortree_rs::prelude::*;
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use nodes::StatusNode;
+use trees::deep;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let deep_tree = deep(100);
+    let mut registry = NodeRegistry::new();
+
+    registry.insert("StatusNode", || StatusNode.to_boxed(), NodeType::Action);
+
+    c.bench_with_input(
+        BenchmarkId::new("parse", "deep-100"),
+        &(deep_tree, registry),
+        |b, (xml, registry)| {
+            b.iter_custom(|iters| {
+                let config = TreeConfig::builder().registry(registry).xml(xml).build();
+
+                let start = Instant::now();
+
+                for _ in 0..iters {
+                    black_box(Tree::from_config(&config).unwrap());
+                }
+
+                start.elapsed()
+            });
+        },
+    );
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
